955f00b73ee217c2b2f312ed062e5cd7ea4e5593
ssr0016 <samsonrecluta0016@gmail.com>
2024-10-24 15:37:32 +0800

    setup routes

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,20 +1,29 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/ssr0016/ecommmerse-app/config"
+	"github.com/ssr0016/ecommmerse-app/internal/api/rest"
+	"github.com/ssr0016/ecommmerse-app/internal/api/rest/handlers"
 )
 
 func StartServer(config config.AppConfig) {
 	app := fiber.New()
 
-	app.Get("/health", healthCheck)
+	rh := &rest.RestHandler{
+		App: app,
+	}
+
+	setupRoutes(rh)
 
 	app.Listen(config.ServerPort)
 }
 
-func healthCheck(ctx *fiber.Ctx) error {
-	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "I'm  am  alive"})
+func setupRoutes(rh *rest.RestHandler) {
+	// user handler
+	handlers.SetupUserRoutes(rh)
+
+	// transactions
+
+	// catalog
 }
