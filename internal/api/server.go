53469554a8c6522ec6efe63c574bcecc87523e15
ssr0016 <samsonrecluta0016@gmail.com>
2024-10-25 13:00:35 +0800

    setup docker for db  and add orm and connect to the database

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,17 +1,34 @@
 package api
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/ssr0016/ecommmerse-app/config"
 	"github.com/ssr0016/ecommmerse-app/internal/api/rest"
 	"github.com/ssr0016/ecommmerse-app/internal/api/rest/handlers"
+	"github.com/ssr0016/ecommmerse-app/internal/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func StartServer(config config.AppConfig) {
 	app := fiber.New()
 
+	// connect to database
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("database connection error %v\n", err)
+	}
+
+	log.Println("database connected successfully!")
+
+	// run migration
+	db.AutoMigrate(&domain.User{})
+
 	rh := &rest.RestHandler{
 		App: app,
+		DB:  db,
 	}
 
 	setupRoutes(rh)
